05000ef9570dde7ed82df74f2d54332e4bb489d9
Chris Wingo and Rafael Perez <rafael@>
2016-10-27 11:20:38 -0700

    Pass 4 specs. Now correctly displays output for numbers 1-39.

--- js/script.js
+++ js/script.js
@@ -36,11 +36,39 @@ function resolveFives (number){
     return result;
   }else {
     result += "V";
-    number -= 5;
-    resolveOnes(number.toString());
+    value -= 5;
+    resolveOnes(value.toString());
   }
   return result;
 }
+
+function resolveTens (number){
+  var value = parseInt(number);
+  var index = parseInt(value / 10);
+
+  for(i = 0; i < index; i++) {
+    if (value % 10 === 0) {
+      value -= 10;
+      result += "X";
+    }else if (value === 0){
+      return result;
+    }else{
+      result += "X";
+      value -= 10;
+      if(value < 3){
+        resolveOnes(value.toString());
+      } else if(value < 9){
+        resolveFives(value.toString());
+      }
+    }
+  }
+  if (value === 9){
+    value -= 9;
+    result += "IX";
+    return result;
+  }
+  return result;
+};
 
 $(document).ready(function(){
   $("form").submit(function(event){
@@ -48,7 +76,7 @@ $(document).ready(function(){
     var userInput = $("#arabicNumerals").val();
     var romanNumeralValues = {"1":"I", "5":"V", "10":"X", "50":"L", "100":"C", "500":"D", "1000":"M"};
     if(checkInteger(userInput)) {
-      result = resolveFives(userInput);
+      result = resolveTens(userInput);
       console.log(result);
       $("#romanNumerals").text(result);
       result = "";
