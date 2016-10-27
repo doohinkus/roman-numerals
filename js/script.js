e8d96757498d7464c43a8edebaeb8563c6bd2c7a
Chris Wingo and Rafael Perez <chris@>
2016-10-27 10:36:53 -0700

    Pass 3 specs. Now display correct outputs for numbers 1-8.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+var result = "";
 
-var checkInteger = function (integer){
-  var input = parseInt(integer);
+var checkInteger = function (number){
+  var input = parseInt(number);
   if (typeof(input) === "number") {
     if (input >= 1 && input <= 3999){
       return true;
@@ -13,7 +14,6 @@ var checkInteger = function (integer){
 };
 
 function resolveOnes(number) {
-  var result = "";
   var value = parseInt(number);
 
   for(i = 0; i < value; i++) {
@@ -23,16 +23,35 @@ function resolveOnes(number) {
   return result;
 };
 
+function resolveFives (number){
+  var value = parseInt(number);
+
+  if (value === 4){
+    result += "IV";
+    value -= 4;
+  }else if(value === 5){
+    result += "V";
+    value -= 5;
+  }else if(value === 0){
+    return result;
+  }else {
+    result += "V";
+    number -= 5;
+    resolveOnes(number.toString());
+  }
+  return result;
+}
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var userInput = $("#arabicNumerals").val();
-    var result = "";
     var romanNumeralValues = {"1":"I", "5":"V", "10":"X", "50":"L", "100":"C", "500":"D", "1000":"M"};
     if(checkInteger(userInput)) {
-      result = resolveOnes(userInput);
+      result = resolveFives(userInput);
       console.log(result);
       $("#romanNumerals").text(result);
+      result = "";
     }
   });
 });
