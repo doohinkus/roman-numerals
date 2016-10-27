d6fbda5c4b04229604294745a83172a8add35686
Chris Wingo and Rafael Perez <rafael@>
2016-10-27 10:00:42 -0700

    Pass the first spec. Check that the value entered is an integer 1-3999

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
+
+var checkInteger = function (integer){
+  var input = parseInt(integer);
+  if (typeof(input) === "number") {
+    if (input >= 1 && input <= 3999){
+      return true;
+    } else {
+        return false;
+      }
+  } else {
+    return false;
+    }
+};
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var userInput = $("#arabicNumerals").val();
     var result = "";
     var romanNumeralValues = {"1":"I", "5":"V", "10":"X", "50":"L", "100":"C", "500":"D", "1000":"M"};
-    console.log(userInput);
+    console.log(checkInteger(userInput));
     console.log(romanNumeralValues["1"]);
     result = romanNumeralValues[userInput];
     console.log(result);
