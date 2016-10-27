c2946a35adddb454e5446e60dd32aa2b85bcfae3
Chris Wingo and Rafael Perez <chris@>
2016-10-27 10:14:06 -0700

    Pass the second spec. Displays roman numeral values in all is

--- js/script.js
+++ js/script.js
@@ -12,16 +12,27 @@ var checkInteger = function (integer){
     }
 };
 
+function resolveOnes(number) {
+  var result = "";
+  var value = parseInt(number);
+
+  for(i = 0; i < value; i++) {
+    result += "I";
+  }
+  console.log(result);
+  return result;
+};
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var userInput = $("#arabicNumerals").val();
     var result = "";
     var romanNumeralValues = {"1":"I", "5":"V", "10":"X", "50":"L", "100":"C", "500":"D", "1000":"M"};
-    console.log(checkInteger(userInput));
-    console.log(romanNumeralValues["1"]);
-    result = romanNumeralValues[userInput];
-    console.log(result);
-    $("#romanNumerals").text(result);
+    if(checkInteger(userInput)) {
+      result = resolveOnes(userInput);
+      console.log(result);
+      $("#romanNumerals").text(result);
+    }
   });
 });
